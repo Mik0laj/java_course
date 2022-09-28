8d9189d554117233f12e52ed0006aa48aaa205b8
unknown <mikolajstyla@wp.pl>
2022-09-28 17:49:04 +0200

    Changed format settings

--- sandbox/src/main/java/sii/stqa/pft/sandbox/Program.java
+++ sandbox/src/main/java/sii/stqa/pft/sandbox/Program.java
@@ -2,8 +2,8 @@ package sii.stqa.pft.sandbox;
 
 public class Program {
 
- public static void main(String[] args){
+  public static void main(String[] args) {
 
-System.out.println("Hello, world!");
-}
+    System.out.println("Hello, world!");
+  }
 }
