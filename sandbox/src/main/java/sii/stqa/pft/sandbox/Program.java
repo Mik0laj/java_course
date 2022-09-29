542d69458205d5bfbfc22f8fd8c0cde5b6e5ffa9
unknown <mikolajstyla@wp.pl>
2022-09-29 20:31:57 +0200

    Added few functions

--- sandbox/src/main/java/sii/stqa/pft/sandbox/Program.java
+++ sandbox/src/main/java/sii/stqa/pft/sandbox/Program.java
@@ -4,6 +4,30 @@ public class Program {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Mikolaj");
+
+    double l = 5;
+    System.out.println("area of square with side " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("area of rectangle with sides " + a + " and " + b + " = " + area(a, b));
+
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 }
