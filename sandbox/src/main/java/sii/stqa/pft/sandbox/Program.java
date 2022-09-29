6d421468a67da85414fc12619226a65eff82084b
unknown <mikolajstyla@wp.pl>
2022-09-29 21:34:57 +0200

    Function distance for task 2.3

--- sandbox/src/main/java/sii/stqa/pft/sandbox/Program.java
+++ sandbox/src/main/java/sii/stqa/pft/sandbox/Program.java
@@ -4,24 +4,17 @@ public class Program {
 
   public static void main(String[] args) {
 
-    hello("world");
-    hello("user");
-    hello("Mikolaj");
-
-    Square s = new Square(5);
-    System.out.println("area of square with side " + s.l + " = " + s.area());
-
-    Rectangle r = new Rectangle(4, 6);
-    System.out.println("area of rectangle with sides " + r.a + " and " + r.b + " = " + r.area());
-
-
+    Point p1= new Point();
+    Point p2=new Point();
+    p1.x=1;
+    p1.y=2;
+    p2.x=7;
+    p2.y=4;
+    System.out.println("Distance bettwen p1 and p2 = " + distance(p1,p2));
   }
 
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-
-  }
-
-
+public static double distance (Point p1, Point p2){
+   return Math.sqrt((Math.pow((p2.x-p1.x),2))+(Math.pow((p2.y-p1.y),2)));
+}
 
 }
