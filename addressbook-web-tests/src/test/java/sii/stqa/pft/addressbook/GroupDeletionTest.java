d10197d577644dd4bc5835332504e4de9ee491f7
unknown <mikolajstyla@wp.pl>
2022-10-07 19:15:57 +0200

    Added Base Class and Group Deletion Test

---
+++ addressbook-web-tests/src/test/java/sii/stqa/pft/addressbook/GroupDeletionTest.java
@@ -0,0 +1,15 @@
+package sii.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest extends TestBase {
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+}
