0efd965b7221028d6db68e40ae39950899b5a563
unknown <mikolajstyla@wp.pl>
2022-10-13 14:10:54 +0200

    Running test on 2 browsers

--- addressbook-web-tests/src/test/java/sii/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/sii/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,11 +1,12 @@
 package sii.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import sii.stqa.pft.addressbook.model.ContactData;
 
 public class ContactHelper extends HelperBase {
-  public ContactHelper(FirefoxDriver wd) {
+  public ContactHelper(WebDriver wd) {
     super(wd);
   }
   public void returnToHomePage() {
--- addressbook-web-tests/src/test/java/sii/stqa/pft/addressbook/test/TestBase.java
+++ addressbook-web-tests/src/test/java/sii/stqa/pft/addressbook/test/TestBase.java
@@ -1,12 +1,13 @@
 package sii.stqa.pft.addressbook.test;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import sii.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
