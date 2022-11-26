1674167a5313149d592c9260e17999343fbaf41a
unknown <mikolajstyla@wp.pl>
2022-11-26 22:26:51 +0100

    Added data provider

--- addressbook-web-tests/src/test/java/sii/stqa/pft/addressbook/test/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/sii/stqa/pft/addressbook/test/ContactCreationTest.java
@@ -1,17 +1,30 @@
 package sii.stqa.pft.addressbook.test;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import sii.stqa.pft.addressbook.model.ContactData;
 import sii.stqa.pft.addressbook.model.Contacts;
 import sii.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
+
+  @DataProvider
+  public Iterator<Object[]> validContacts(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new ContactData().withFirstName("firstName1").withLastName("lastname1").withAddress("address1")});
+    list.add(new Object[]{new ContactData().withFirstName("firstName2").withLastName("lastname2").withAddress("address2")});
+    list.add(new Object[]{new ContactData().withFirstName("firstName3").withLastName("lastname3").withAddress("address3")});
+    return list.iterator();
+  }
 
   @BeforeMethod
   public void ensurePreconditions() {
@@ -22,11 +35,11 @@ public class ContactCreationTest extends TestBase {
     app.goTo().homePage();
   }
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/pl.png");
-    ContactData contact = new ContactData().withFirstName("Name1").withLastName("Surname2").withPhoto(photo);
+    contact.withPhoto(photo);
     app.contact().create(contact);
     assertThat(app.contact().count(), equalTo(before.size()+1));
     Contacts after = app.contact().all();
