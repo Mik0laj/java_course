8348aa7c4640280db20bf08f350124f9f74fd798
unknown <mikolajstyla@wp.pl>
2022-11-27 13:07:48 +0100

    Added loading test data from file

--- addressbook-web-tests/src/test/java/sii/stqa/pft/addressbook/test/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/sii/stqa/pft/addressbook/test/ContactCreationTest.java
@@ -7,7 +7,10 @@ import sii.stqa.pft.addressbook.model.ContactData;
 import sii.stqa.pft.addressbook.model.Contacts;
 import sii.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,11 +21,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTest extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts(){
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{new ContactData().withFirstName("firstName1").withLastName("lastname1").withAddress("address1")});
-    list.add(new Object[]{new ContactData().withFirstName("firstName2").withLastName("lastname2").withAddress("address2")});
-    list.add(new Object[]{new ContactData().withFirstName("firstName3").withLastName("lastname3").withAddress("address3")});
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.csv"));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withFirstName(split[0]).withLastName(split[1]).withAddress(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
